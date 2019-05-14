1e4973ddb724d7e31a853497081649412ca95150
a9210 <test@test.com>
2019-05-15 00:32:55 +0900

    agent returns return code and @invoke read and return that #6

--- shrimp-oo-agent.sh
+++ shrimp-oo-agent.sh
@@ -16,6 +16,7 @@ do
         fi
         echo "" > ${FIFO_STDOUT}        # dummy output for avoiding client halt when this method has no output
         eval ${line} > ${FIFO_STDOUT}
+        echo "$?" > ${FIFO_STDOUT}      # invoked function's return value
     done < ${FIFO_STDIN}
 done
 
