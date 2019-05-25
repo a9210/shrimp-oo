e24ec1ac240b69bad6f598e7b178d99db45bd70f
a9210 <test@test.com>
2019-05-26 00:29:30 +0900

    marge commands that redirects #6

--- shrimp-oo-agent.sh
+++ shrimp-oo-agent.sh
@@ -14,9 +14,11 @@ do
             LOOP_FLAG=false
             break
         fi
-        echo "" >> ${FIFO_STDOUT}        # dummy output for avoiding client halt when this method has no output
-        eval ${line} >> ${FIFO_STDOUT}
-        echo "$?" >> ${FIFO_STDOUT}      # send invoked function's return value
+        {echo "";eval ${line};echo "$?";} >> ${FIFO_STDOUT}        # dummy output for avoiding client halt when this method has no output
+
+#        echo "" >> ${FIFO_STDOUT}        # dummy output for avoiding client halt when this method has no output
+#        eval ${line} >> ${FIFO_STDOUT}
+#        echo "$?" >> ${FIFO_STDOUT}      # send invoked function's return value
     done < ${FIFO_STDIN}
 done
 
