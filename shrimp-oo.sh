58e2bb62a19efd5945b8d0b034701c436adc157d
a9210 <test@test.com>
2019-12-28 22:58:06 +0900

    add @flow

--- shrimp-oo.sh
+++ shrimp-oo.sh
@@ -62,6 +62,47 @@ function @invoke() {
 
     return $((previusLine))
 }
+function @flow() {
+    local METHOD=$(echo $1 | awk -F. '{print $NF;}')
+    local OBJ_PID=$(echo $1 | sed "s/\.${METHOD}$//")
+    shift
+    local OBJ_STDIN="${OBJ_PID}_stdin.fifo"
+    local OBJ_STDOUT="${OBJ_PID}_stdout.fifo"
+
+    local INVOKE_LINE="${METHOD} "
+    for arg in "$@"
+    do
+        INVOKE_LINE="${INVOKE_LINE} \"${arg}\""
+    done
+
+    # invoke
+    echo ${INVOKE_LINE} >> "${OBJ_STDIN}"
+
+    # get output
+    local IS_FIRST_LINE=true
+    local isSecondLine=true
+    local previusLine
+    while read line
+    do
+        # First line is always dummy.
+        if ${IS_FIRST_LINE}
+        then
+            IS_FIRST_LINE=false
+            continue
+        fi
+
+        if ${isSecondLine}
+        then
+            isSecondLine=false
+            previusLine=${line}
+            continue
+        fi
+        echo ${previusLine}
+        previusLine=${line}
+    done < "${OBJ_STDOUT}"
+
+    return $((previusLine))
+}
 function @delete {
     local OBJ_PID=$1
     echo "@delete" >> "${OBJ_PID}_stdin.fifo"
